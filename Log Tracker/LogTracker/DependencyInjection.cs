d830e5a45a33ff96e45966d7af248d91e249460f
DipuKP2001 <dipukp602@gmail.com>
2025-06-11 15:19:16 +0530

    Log Tracker base structure

---
+++ Log Tracker/LogTracker/DependencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LogTracker;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
