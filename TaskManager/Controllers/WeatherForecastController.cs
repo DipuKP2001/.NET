89b818ad5ed4bdf4bf8ba40f441c8d1f8cd2fe03
Dipu K P <dipu.kp@soti.net>
2026-01-17 23:44:26 +0530

    Dependency Injection

---
+++ TaskManager/Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Services;
+
+namespace TaskManager.Controllers;
+
+[ApiController]
+[Route("weatherforecast")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly IWeatherForecastService _service;
+
+    public WeatherForecastController(IWeatherForecastService service)
+    {
+        _service = service;
+    }
+    
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var forecast = _service.GetForecasts();
+        return Ok(forecast);
+    }
+}
---
+++ TaskManager/Services/WeatherForecastService.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskManager.Services;
+
+public class WeatherForecastService : IWeatherForecastService
+{
+    Guid _guid = Guid.NewGuid();
+    
+    private static readonly string[] Forecasts = new[]
+    {
+        "Sunny", "Cloudy", "Rainy", "Windy", "Stormy"
+    };
+    
+    public IEnumerable<string> GetForecasts()
+    {
+        Console.WriteLine($"[WeatherForecastService] Instance ID: {GetHashCode()} GUID: {_guid}");
+        return Forecasts;
+    }
+}
